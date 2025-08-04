727c7c604413e2cf0a43382f98fda227064f0250
Jhon Edwin Orrego <jhonedw42@gmail.com>
2025-08-03 20:07:03 -0500

    Mi primer programa: Hola mundo

---
+++ Hola-mundo.py
@@ -0,0 +1,7 @@
+# Mi primer programa en Python
+print("¡Hola mundo desde GitHub!")
+print("Este es mi primer commit")
+
+# Información personal
+nombre = "Edwin"
+print(f"Mi nombre es {nombre} y estoy aprendiendo a programar")
